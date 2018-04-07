1f6d0cf2c0e7334aad066f0da8e4aed2c545d59d
AE <englehorn@gmail.com>
2018-04-07 14:58:16 -0600

    changed load time on launch screen

--- genera-swift/AppDelegate.swift
+++ genera-swift/AppDelegate.swift
@@ -47,6 +47,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
+        // Change load time on launch screen
+        Thread.sleep(forTimeInterval: 2);
+        
         //Check for database
        
         
